7a8f75fbb0c7862bde46d720bb824fc1603d61a3
Eva Maria Alonso Medina <evam.alonso95@gmail.com>
2024-11-29 17:49:52 +0100

    bbdd

---
+++ bbdd.php
@@ -0,0 +1,10 @@
+<?php 
+class Database
+{
+    private $host = "localhost";
+    private $username = "root"; // Cambia esto por tu nombre de usuario de la base de datos
+    private $password = ""; // Cambia esto por tu contraseña de la base de datos
+    private $database = "juegos"; // Cambia esto por el nombre de tu base de datos
+   
+
+}
