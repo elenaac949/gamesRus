3637538d011177bac50e0896ed77ff0eef9d04b1
Elena A.C. <161213022+elenaac949@users.noreply.github.com>
2024-12-23 19:55:52 +0100

    css perfil

--- frm/frm_perfil_usuario.php
+++ frm/frm_perfil_usuario.php
@@ -9,67 +9,68 @@ include __DIR__ . '/../common/controlSesion.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil de Usuario</title>
     <link rel="stylesheet" href="/gamesRus/css/general.css">
+    <link rel="stylesheet" href="/gamesRus/css/frm_perfil.css">
 </head>
 
 <body>
     <?php
     include './common/cabecera.php';
     ?>
-    <section class="actualizar_datos">
-        <h2>Actualizar Datos de Usuario</h2>
-        <form action="" method="post">
-            <p class="errores"><?= $error ?></p>
-            <?php /* var_dump($data) */ ?>
-            <fieldset>
-                <legend>Credenciales</legend>
-                <input required type="text" name="alias" id="alias" placeholder="Nick o Alias*" value="<?php echo $data['nick']; ?>">
-                <input required type="email" name="correo" id="correo" placeholder="Correo Electrónico*" value="<?php echo $data['email']; ?>">
+    <main>
+        <section class="actualizar_datos">
+            <h2>Actualizar Datos de Usuario</h2>
+            <form action="" method="post">
+                <p class="errores"><?= $error ?></p>
+                <?php /* var_dump($data) */ ?>
+                <fieldset>
+                    <legend>Credenciales</legend>
+                    <input required type="text" name="alias" id="alias" placeholder="Nick o Alias*" value="<?php echo $data['nick']; ?>">
+                    <input required type="email" name="correo" id="correo" placeholder="Correo Electrónico*" value="<?php echo $data['email']; ?>">
 
-            </fieldset>
-            <fieldset>
-                <legend>Datos personales</legend>
-                <input required type="text" name="nombre" placeholder="Nombre*" value="<?php echo $data['nombre']; ?>">
-                <input required type="text" name="apellidos" placeholder="Apellidos*" value="<?php echo $data['apellidos']; ?>">
+                </fieldset>
+                <fieldset>
+                    <legend>Datos personales</legend>
+                    <input required type="text" name="nombre" placeholder="Nombre*" value="<?php echo $data['nombre']; ?>">
+                    <input required type="text" name="apellidos" placeholder="Apellidos*" value="<?php echo $data['apellidos']; ?>">
 
-            </fieldset>
-            <fieldset>
-                <legend>Datos de contacto</legend>
-                <input required type="text" name="tipo_via" placeholder="Tipo de vía" value="<?php echo $data['TipoDeVia']; ?>">
-                <input required type="text" name="nombre_via" placeholder="Nombre de la vía" value="<?php echo $data['NombreDeVia']; ?>">
-                <input required type="text" name="numero_via" placeholder="Número Vía" value="<?php echo $data['Numero']; ?>">
-                <input required type="text" name="numeros" placeholder="Número Piso" value="<?php echo $data['Numeros']; ?>">
-                <input required type="text" name="otros" placeholder="Otros datos" value="<?php echo $data['Otros']; ?>">
-                <input required type="tel" name="telefono" placeholder="Teléfono*" value="<?php echo $data['NumeroTelefono']; ?>">
+                </fieldset>
+                <fieldset>
+                    <legend>Datos de contacto</legend>
+                    <input required type="text" name="tipo_via" placeholder="Tipo de vía" value="<?php echo $data['TipoDeVia']; ?>">
+                    <input required type="text" name="nombre_via" placeholder="Nombre de la vía" value="<?php echo $data['NombreDeVia']; ?>">
+                    <input required type="text" name="numero_via" placeholder="Número Vía" value="<?php echo $data['Numero']; ?>">
+                    <input required type="text" name="numeros" placeholder="Número Piso" value="<?php echo $data['Numeros']; ?>">
+                    <input required type="text" name="otros" placeholder="Otros datos" value="<?php echo $data['Otros']; ?>">
+                    <input required type="tel" name="telefono" placeholder="Teléfono*" value="<?php echo $data['NumeroTelefono']; ?>">
 
-            </fieldset>
+                </fieldset>
 
-            <input type="submit" name="btn_actualizar_datos" value="Actualizar Datos" class="actualizar">
+                <input type="submit" name="btn_actualizar_datos" value="Actualizar Datos" class="actualizar">
 
-        </form>
-    </section>
+            </form>
+        </section>
 
-    <section class="tarjetas">
-        <!-- mostrar tarjetas de credito -->
-         <h2>Tus tarjetas</h2>
-         
-    </section>
+        <section class="tarjetas">
+            <!-- mostrar tarjetas de credito -->
+            <h2>Tus tarjetas</h2>
 
+        </section>
 
-    
-
-    <section class="borrar_cuenta">
-        <!-- si la sesion no es del acministrador podra eliminar su cuenta -->
-        <h2>Eliminar Cuenta de usuario</h2>
-        <form action="" method="post">
-            <?php if ($_SESSION['nickUsuario'] == 'admin') : ?>
-                <p>No puedes eliminar esta cuenta</p>
-            <?php else : ?>
-                <input type="submit" name="btn_eliminar_cuenta" value="Eliminar Cuenta">
-            <?php endif; ?>
-        </form>
+        <section class="borrar_cuenta">
+            <!-- si la sesion no es del acministrador podra eliminar su cuenta -->
+            <h2>Eliminar Cuenta de usuario</h2>
+            <form action="" method="post">
+                <?php if ($_SESSION['nickUsuario'] == 'admin') : ?>
+                    <p>No puedes eliminar esta cuenta</p>
+                <?php else : ?>
+                    <input type="submit" name="btn_eliminar_cuenta" value="Eliminar Cuenta">
+                <?php endif; ?>
+            </form>
 
 
-    </section>
+        </section>
+    </main>
+
 
 
 
