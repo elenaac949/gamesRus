932cfb43ba276b816da072cb5d1c3486bb2b9a8e
Elena A.C. <161213022+elenaac949@users.noreply.github.com>
2025-02-18 23:50:23 +0100

    final

--- common/sobreNosotras.php
+++ common/sobreNosotras.php
@@ -15,7 +15,7 @@
 
     <main class="container">
         <h1>Sobre Nosotros</h1>
-        <img src="/gamesRus/img/shakira.jpegs" alt="Shakira con ordenador." class="imagen-equipo">
+        <img src="/gamesRus/img/shakira.jpeg" alt="Shakira con ordenador." class="imagen-equipo">
     </main>
 
     <?php include './common/footer.php'; ?>
