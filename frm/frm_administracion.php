e1630efd1093c66fcfa341c23237f4d11fb59f33
Eva Maria Alonso Medina <evam.alonso95@gmail.com>
2025-02-27 12:03:34 +0100

    quitar las bbdd antiguas

--- frm/frm_administracion.php
+++ frm/frm_administracion.php
@@ -197,15 +197,10 @@ include __DIR__ . '/../common/controlSesion.php';
                         for (let i = 0; i < Math.min(juego.genres.length, 5); i++) {
                             markSelectedCheckboxes("genero_juego", juego.genres[i].genre_id);
                         }
-                        // document.querySelector("#genero_juego").value = juego.genres[0].genre_name
-                        // document.querySelector("#desarrollador_juego").value = juego.developers[0].name;
                         document.querySelector("#anio_lanzamiento").value = juego.platforms[0].first_release_date;
-                        //SISTEMA ES ESTO
                         for (let i = 0; i < Math.min(juego.platforms.length, 5); i++) {
                             markSelectedCheckboxes("sistema_juego", juego.platforms[i].platform_id);
                         }
-                        //document.querySelector("#distribuidor_juego").value = juego.platforms[0].platform_name;
-                        // document.querySelector("#ruta_juego").value = juego.url;
                         document.querySelector("#descripcion_juego").value = removeHTMLTags(juego.description);
                         document.querySelector("#portada_juego").value = juego.sample_cover.image;
                     });
